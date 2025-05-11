5e39ce74e8cb123a004e4b3a1108d0f5053e400d
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-12 02:56:31 +0900

    Update twrp_szj203.mk

--- twrp_szj203.mk
+++ twrp_szj203.mk
@@ -30,8 +30,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.build.product=SZJ-JS203 \
     ro.product.board=k65v1_64_bsp \
     ro.product.device=SZJ203 \
-    persist.sys.usb.config=mtp
-
+    persist.sys.usb.config=adb
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vnd_k65v1_64_bsp-user 12 SP1A.210812.016 20231108 release-keys"
 
