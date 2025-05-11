4c6f41baff8c58af043720012c93152df2cfc3ca
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-11 14:14:52 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/vortex/CG65
+LOCAL_PATH := device/kyocera/szj203
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
