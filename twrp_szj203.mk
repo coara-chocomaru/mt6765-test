fe6d2795307675b83e0ca97f469fc566477baa6c
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-16 00:43:53 +0900

    Update twrp_szj203.mk

--- twrp_szj203.mk
+++ twrp_szj203.mk
@@ -29,11 +29,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-kyocera
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.build.product=SZJ-JS203 \
     ro.product.board=k65v1_64_bsp \
-    ro.product.device=SZJ203 \
-    persist.service.adb.enable=1 \
-　　 persist.service.debuggable=1 \
-    persist.sys.usb.config=mtp,adb \
-    ro.usb.config=mtp,adb
+    ro.product.device=SZJ203
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vnd_k65v1_64_bsp-user 12 SP1A.210812.016 20231108 release-keys"
 
