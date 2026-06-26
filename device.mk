f3bb5034142bb4feb109872f23e965ae88a8ff37
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-06-26 12:55:07 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -44,8 +44,8 @@ PRODUCT_PACKAGES += \
     libz \
     libcutils \
     fastbootd \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
+    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock.recovery
 
 PRODUCT_PACKAGES += \
     bootctrl \
